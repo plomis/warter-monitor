9bf9df8fd4b4d826ceff9daf2db3bd9966b6c8ff
jokemmy <252133226@qq.com>
2019-10-18 16:08:00 +0800

    删老项目

--- WaterMonitor/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [ 'import', { libraryName: '@ant-design/react-native' }],
-    [ 'inline-import', { extensions: [ '.jst', '.tpl', '.svg' ]}]
-  ]
-};
